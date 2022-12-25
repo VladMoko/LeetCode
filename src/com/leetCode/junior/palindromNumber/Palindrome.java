de8f9caa412910b1edaaf9655184090c7f0a7aa9
m <advmoney2233@gmail.com>
2022-12-25 21:01:55 +0200

    second task from LeetCode

---
+++ src/com/leetCode/junior/palindromNumber/Palindrome.java
@@ -0,0 +1,24 @@
+package com.leetCode.junior.palindromNumber;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        //store the number to originalNumber
+        int reversNumber = 0;
+        int remainder;
+        int originalNumber = x;
+        //get the revers of originalNumber
+        while (x != 0) {
+            remainder = x % 10;
+            reversNumber = reversNumber * 10 + remainder;
+            x /= 10;
+        }
+        //check if reversNumber equals originalNumber
+        //return true else false
+        return originalNumber == reversNumber;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("solution.isPalindrome(10) = " + solution.isPalindrome(10));
+        System.out.println("solution.isPalindrome(121) = " + solution.isPalindrome(121));
+    }
+}
